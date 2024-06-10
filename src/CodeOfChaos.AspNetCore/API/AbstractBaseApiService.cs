1d044f730425f7a47a13c13240264169df20c4d3
AndreasSasDev <directive.athena@gmail.com>
2024-06-10 17:59:02 +0200

    Feat: AbstractBaseApiService

---
+++ src/CodeOfChaos.AspNetCore/API/AbstractBaseApiService.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeOfChaos.AspNetCore.API;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+
+/// <summary>
+/// Represents an abstract base API service.
+/// </summary>
+/// <typeparam name="TDbContext">The type of the entity framework database context.</typeparam>
+public abstract class AbstractBaseApiService<TDbContext>(
+    IDbContextFactory<TDbContext> contextFactory
+    ) where TDbContext : DbContext {
+
+    /// <summary>
+    /// Represents the database context in Entity Framework Core.
+    /// </summary>
+    [UsedImplicitly]
+    protected Task<TDbContext> DbContext => contextFactory.CreateDbContextAsync();
+}

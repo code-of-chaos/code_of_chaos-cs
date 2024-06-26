1daa8a64096b6bb9a988dad0a36aebe2185691f0
AndreasSasDev <directive.athena@gmail.com>
2024-06-26 17:10:20 +0200

    Feat(CodeOfChaos.Lucide): Update to Lucide 0.397.0 & Tests

---
+++ src/CodeOfChaos.Tests/Lucide/IconSetGeneratedTest.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Lucide;
+using JetBrains.Annotations;
+using System;
+using Xunit;
+
+namespace CodeOfChaos.Tests.Lucide;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[TestSubject(typeof(IconSet))]
+public class IconSetGeneratedTest {
+   [Fact]
+   public void IconsExpectedAmount() {
+      // Arrange
+      const int expectedCount = LucideData.AmountOfIcons;
+      
+      // Act
+      int actualNumberOfValues = IconSet.IconAtlas.Count;
+      
+      // Assert
+      Assert.Equal(expectedCount, actualNumberOfValues);
+   }
+}
